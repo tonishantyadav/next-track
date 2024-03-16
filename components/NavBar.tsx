1c4a713b4273bf8b9d0f2c6da8b8256d37a5b65d
Nishant Yadav <inishantyadav24@gmail.com>
2024-03-16 12:06:17 +0530

    Styling NavLinks

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,27 +1,39 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { FaBug } from "react-icons/fa";
+import classNames from "classnames";
 
 const NavBar = () => {
   const otherLinks = [
     { label: "Dashboard", href: "/dashboard" },
     { label: "Issues", href: "/issues" },
   ];
+  const path = usePathname();
 
   return (
     <nav className="flex gap-10 sm:text-lg md:text-xl lg:text-xl p-5 border-b">
-      <ul className="flex items-center gap-2">
+      <ul className="flex items-center gap-2 text-slate-200">
         <li className="mr-2 hover:text-slate-400">
           <Link href="/">
-            <FaBug /> 
+            <FaBug />
           </Link>
         </li>
         <li className="pt-1 hover:text-slate-400">
           <Link href="/">Next Track</Link>
         </li>
       </ul>
-      <ul className="flex items-center gap-5 pt-1">
+      <ul className="flex items-center gap-5 pt-1 text-slate-200">
         {otherLinks.map(({ label, href }) => (
-          <li className="hover:text-slate-400" key={href}>
+          <li
+            className={classNames({
+              "underline decoration-primary hover:text-slate-400":
+                path === href,
+              "hover:text-slate-400": true,
+            })}
+            key={href}
+          >
             <Link href={href}>{label}</Link>
           </li>
         ))}
